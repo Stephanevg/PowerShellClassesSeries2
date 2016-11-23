b9a772beb3ff35ec8da381cd9b3065e6214ff218
stephanevg <svangulick@gmail.com>
2016-11-23 01:09:51 +0100

    added classes_part1.ps1

---
+++ Classes_part1.ps1
@@ -0,0 +1,19 @@
+﻿$Properties = @{
+  “FirstName” = “Stéphane”
+  “LastName” = “van Gulick”
+  “WebSite” = “www.powershelldistrict.com”
+}
+$Object = New-object -TypeName psobject -Property $properties
+$Object
+
+Class Author {
+
+    $FirstName = 'Stéphane'
+    $LastName = "van Gulick"
+    $WebSite = 'www.powershelldistrict.Com'
+
+}
+
+New-Object Author
+
+[Author]::new()
